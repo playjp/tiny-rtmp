917f57e152004bb44a6ae7c7153b0fcd26b29efa
Taichiro Ishikawa <taichiro.ishikawa+github@play.jp>
2025-08-22 13:09:40 +0900

    chore: add test coverage support with Vitest

---
+++ vitest.config.mjs
@@ -0,0 +1,14 @@
+
+import { defineConfig, coverageConfigDefaults } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    coverage: {
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        '**/*.js',
+        '**/*.mjs',
+      ],
+    },
+  },
+});
